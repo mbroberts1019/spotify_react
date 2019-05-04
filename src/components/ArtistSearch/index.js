cf859c1f4cf948b6bae8d5569906fc64a8f37bfd
Mike Roberts <mikebroberts@hotmail.com>
2019-05-04 13:48:22 -0600

    Initial commit header is showing up

---
+++ src/components/ArtistSearch/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+
+export default class ArtistSearch extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <h1>Here is where we will put 1st search call </h1>
+        
+      </div>
+
+
+
+    )
+  }
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+
+export default class Header extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <h1>This is Our Header Div </h1>
+      </div>
+
+
+
+    )
+  }
+}
