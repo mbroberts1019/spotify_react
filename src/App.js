fdd8a577c9d796aee334535f54800dbcbca9c0a2
Isaac German <isaacgerman95@gmail.com>
2019-05-05 10:44:19 -0600

    Styling of the search album art component.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/Header';
+import Header from './components/header';
 import ArtistSearch from './components/ArtistSearch';
-
+import AlbumSearch from './components/AlbumSearch';
 
 function App() {
-
-
-
-
 
   return (
     <div className="App">
       <Header />
+      <AlbumSearch />
+      <ArtistSearch />
     </div>
   );
 }
