47974ffe6f7310c1545078fbaddac658825da8f4
Mike Roberts <mikebroberts@hotmail.com>
2019-05-05 11:11:13 -0600

    fixing merge missing files

--- src/components/ArtistSearch.js
+++ src/components/ArtistSearch.js
@@ -2,14 +2,57 @@ import React from 'react';
 
 
 
+const artistSearch = {
+  color: 'rgba(41, 184, 12)',
+  height: '20rem',
+  backgroundColor: '#B00B55',
+  textAlign: 'center',
+  paddingTop: '0.5rem',
+  margin: 'auto'
+  
+}
+
+const h2Style ={
+  padding: '2rem',
+  margin: 'auto',
+  fontSize: '5rem'
+}
+
+
+
 
 export default class ArtistSearch extends React.Component{
+  state = {
+    artistPics: [],
+    artistNames:[]
+  }
+  
+  onSubmit(e) {
+    e.preventDefault();
+    let artist =document.getElementById("searchedName").value;
+    const urlParams = new URLSearchParams(window.location.hash.replace('#', '?'));
+    const access_token = urlParams.get('access_token')
 
+    fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(artist)}&type=track%2Cartist&market=US&limit=5&offset=0`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + access_token
+      }
+    }).then((res) => res.json())
+    .then(res => {
+      console.log("RES", res);});
+  }
   render(){
     return(
-      <div>
-        <h1>Here is where we will put 1st search call </h1>
-        
+      <div style= {artistSearch}>
+        <h2 style= {h2Style}>Search an Artist:</h2>
+        <form onSubmit= {(e)=>this.onSubmit(e)}>
+          <input id= "searchedName" type= "text" placeholder="Artist Name" ></input>
+          <button type= "submit">Submit</button>
+        </form>
+
       </div>
 
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
+const headerStyle = {
+  color: 'rgb(176, 34, 219)',
+  backgroundColor: 'rgb(22, 219, 226)',
+  border: '1px',
+  height: '18rem',
+  textAlign: 'center',
+};
+
+const h1Style = {
+  padding: '2rem',
+  margin: 'auto',
+  fontSize: '5rem'
+}
 
 
 
 export default class Header extends React.Component{
 
   render(){
-    return(
-      <div>
-        <h1>This is Our Header Div </h1>
-      </div>
+    
 
+    return(
+      <div style= {headerStyle}>
+        <h1 style= {h1Style}>Welcome to the Spotify Mood Checker </h1>
+      </div>
+      
+      
 
 
     )
