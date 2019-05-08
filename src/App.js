db46200defc26952af8bf608b39170e991a83cba
Mike Roberts <mikebroberts@hotmail.com>
2019-05-07 22:02:36 -0600

    header.js to Header.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/header';
+import Header from './components/Header';
 import ArtistSearch from './components/ArtistSearch';
 import SpotifyLogin from './components/SpotifyLogin';
 import AlbumSearch from './components/AlbumSearch';
